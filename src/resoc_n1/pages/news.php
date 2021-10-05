a4ec7158011ba0eb9f716baa391360f166f094c4
Noémie Ragot <noemieprog@gmail.com>
2021-10-05 09:56:14 +0200

    localhost1889

--- src/resoc_n1/pages/news.php
+++ src/resoc_n1/pages/news.php
@@ -45,7 +45,7 @@
                  */
 
                 // Etape 1: Ouvrir une connexion avec la base de donnée.
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                $mysqli = new mysqli("localhost:1889", "root", "root", "socialnetwork");
                 //verification
                 if ($mysqli->connect_errno)
                 {
